25c445a55ca233552b76c719bea7e05caef824b9
mmc00 <dracomarmol@gmail.com>
2026-02-04 10:36:16 -0600

    chore: update package exports
    Update __init__.py to export:
    - Model, Block, register_block
    - Core classes: Set, SetManager, Parameter, Variable, Equation
    - All public API components
    
    Makes framework components easily accessible.

--- src/equilibria/__init__.py
+++ src/equilibria/__init__.py
@@ -1,5 +1,24 @@
 """equilibria - A Modern Python Framework for CGE Modeling."""
 
+from equilibria.blocks import Block, register_block
+from equilibria.core import (
+    Equation,
+    Parameter,
+    Set,
+    SetManager,
+    Variable,
+)
+from equilibria.model import Model
 from equilibria.version import __version__
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "Model",
+    "Block",
+    "register_block",
+    "Set",
+    "SetManager",
+    "Parameter",
+    "Variable",
+    "Equation",
+]
