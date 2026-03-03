0b31b39a64b7905a05eb4d9782e19f5e9ae4d4b4
mmc00 <dracomarmol@gmail.com>
2026-03-03 14:18:11 -0600

    Remove legacy PEP template route and update docs

--- src/equilibria/templates/__init__.py
+++ src/equilibria/templates/__init__.py
@@ -12,16 +12,11 @@ from equilibria.templates.gams_comparison import (
     SolutionComparator,
     run_gams_comparison,
 )
-from equilibria.templates.pep import PEP1R, PEPBaseTemplate, PEPSetManager
 from equilibria.templates.simple_open import SimpleOpenEconomy
 
 __all__ = [
     "ModelTemplate",
     "SimpleOpenEconomy",
-    # PEP Templates
-    "PEPBaseTemplate",
-    "PEP1R",
-    "PEPSetManager",
     # GAMS Comparison
     "GAMSComparisonResult",
     "GAMSComparisonReport",
