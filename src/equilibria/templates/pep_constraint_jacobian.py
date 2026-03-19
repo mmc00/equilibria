77f234c0dbcdb801933f32271c35171da3fce0b2
mmc00 <dracomarmol@gmail.com>
2026-03-19 07:17:27 -0600

    Expand analytic Jacobian coverage for PEP identities

--- src/equilibria/templates/pep_constraint_jacobian.py
+++ src/equilibria/templates/pep_constraint_jacobian.py
@@ -72,8 +72,9 @@ class PEPConstraintJacobianHarness:
             if analytic is None:
                 continue
             analytic_rows.add(row)
+            scale = float(self._constraint_scale[row]) if self._constraint_scale is not None else 1.0
             for col, value in analytic.items():
-                jac[row, col] = value
+                jac[row, col] = value / scale
 
         fd_eps = np.sqrt(np.finfo(float).eps)
         for i in range(n):
@@ -170,6 +171,37 @@ class PEPConstraintJacobianHarness:
             add(f"PP[{j}]", -(1.0 + ttip))
             return result
 
+        if constraint_name.startswith("EQ65_"):
+            j = constraint_name.split("_", 1)[1]
+            add(f"PP[{j}]", vars.XST.get(j, 0.0))
+            add(f"XST[{j}]", vars.PP.get(j, 0.0))
+            add(f"PVA[{j}]", -vars.VA.get(j, 0.0))
+            add(f"VA[{j}]", -vars.PVA.get(j, 0.0))
+            add(f"PCI[{j}]", -vars.CI.get(j, 0.0))
+            add(f"CI[{j}]", -vars.PCI.get(j, 0.0))
+            return result
+
+        if constraint_name.startswith("EQ67_"):
+            j = constraint_name.split("_", 1)[1]
+            add(f"PCI[{j}]", vars.CI.get(j, 0.0))
+            add(f"CI[{j}]", vars.PCI.get(j, 0.0))
+            for i in self.sets.get("I", []):
+                add(f"PC[{i}]", -vars.DI.get((i, j), 0.0))
+                add(f"DI[{i},{j}]", -vars.PC.get(i, 0.0))
+            return result
+
+        if constraint_name.startswith("EQ68_"):
+            j = constraint_name.split("_", 1)[1]
+            add(f"PVA[{j}]", vars.VA.get(j, 0.0))
+            add(f"VA[{j}]", vars.PVA.get(j, 0.0))
+            if any(abs(self.equations.params.get("LDO0", {}).get((l, j), 0.0)) > 1e-12 for l in self.sets.get("L", [])):
+                add(f"WC[{j}]", -vars.LDC.get(j, 0.0))
+                add(f"LDC[{j}]", -vars.WC.get(j, 0.0))
+            if any(abs(self.equations.params.get("KDO0", {}).get((k, j), 0.0)) > 1e-12 for k in self.sets.get("K", [])):
+                add(f"RC[{j}]", -vars.KDC.get(j, 0.0))
+                add(f"KDC[{j}]", -vars.RC.get(j, 0.0))
+            return result
+
         if constraint_name.startswith("EQ70_"):
             _, l, j = constraint_name.split("_", 2)
             ttiw = self.equations.params.get("ttiw", {}).get((l, j), 0.0)
@@ -194,6 +226,61 @@ class PEPConstraintJacobianHarness:
             _, j, i = constraint_name.split("_", 2)
             add(f"P[{j},{i}]", 1.0)
             add(f"PT[{j}]", -1.0)
+            return result
+
+        if constraint_name.startswith("EQ75_"):
+            _, j, i = constraint_name.split("_", 2)
+            add(f"P[{j},{i}]", vars.XS.get((j, i), 0.0))
+            add(f"XS[{j},{i}]", vars.P.get((j, i), 0.0))
+            if abs(self.equations.params.get("EXO0", {}).get((j, i), 0.0)) > 1e-12:
+                add(f"PE[{i}]", -vars.EX.get((j, i), 0.0))
+                add(f"EX[{j},{i}]", -vars.PE.get(i, 0.0))
+            if abs(self.equations.params.get("DSO0", {}).get((j, i), 0.0)) > 1e-12:
+                add(f"PL[{i}]", -vars.DS.get((j, i), 0.0))
+                add(f"DS[{j},{i}]", -vars.PL.get(i, 0.0))
+            return result
+
+        if constraint_name.startswith("EQ76_"):
+            i = constraint_name.split("_", 1)[1]
+            ttix = self.equations.params.get("ttix", {}).get(i, 0.0)
+            add(f"PE[{i}]", 1.0)
+            add(f"PE_FOB[{i}]", -(1.0 / (1.0 + ttix)))
+            for ij in self.sets.get("I", []):
+                add(f"PC[{ij}]", self.equations.params.get("tmrg_X", {}).get((ij, i), 0.0))
+            return result
+
+        if constraint_name.startswith("EQ77_"):
+            i = constraint_name.split("_", 1)[1]
+            ttic = self.equations.params.get("ttic", {}).get(i, 0.0)
+            factor = 1.0 + ttic
+            add(f"PD[{i}]", 1.0)
+            add(f"PL[{i}]", -factor)
+            for ij in self.sets.get("I", []):
+                add(f"PC[{ij}]", -(factor * self.equations.params.get("tmrg", {}).get((ij, i), 0.0)))
+            return result
+
+        if constraint_name.startswith("EQ78_"):
+            i = constraint_name.split("_", 1)[1]
+            ttim = self.equations.params.get("ttim", {}).get(i, 0.0)
+            ttic = self.equations.params.get("ttic", {}).get(i, 0.0)
+            factor = (1.0 + ttic) * (1.0 + ttim)
+            add(f"PM[{i}]", 1.0)
+            add("e", -(factor * vars.PWM.get(i, 0.0)))
+            add(f"PWM[{i}]", -(factor * vars.e))
+            for ij in self.sets.get("I", []):
+                add(f"PC[{ij}]", -((1.0 + ttic) * self.equations.params.get("tmrg", {}).get((ij, i), 0.0)))
+            return result
+
+        if constraint_name.startswith("EQ79_"):
+            i = constraint_name.split("_", 1)[1]
+            add(f"PC[{i}]", vars.Q.get(i, 0.0))
+            add(f"Q[{i}]", vars.PC.get(i, 0.0))
+            if abs(self.equations.params.get("IMO0", {}).get(i, 0.0)) > 1e-12:
+                add(f"PM[{i}]", -vars.IM.get(i, 0.0))
+                add(f"IM[{i}]", -vars.PM.get(i, 0.0))
+            if abs(self.equations.params.get("DDO0", {}).get(i, 0.0)) > 1e-12:
+                add(f"PD[{i}]", -vars.DD.get(i, 0.0))
+                add(f"DD[{i}]", -vars.PD.get(i, 0.0))
             return result
 
         if constraint_name == "EQ81":
@@ -254,6 +341,58 @@ class PEPConstraintJacobianHarness:
                 add(f"PC[{i}]", -(expected * g / pc_i))
             return result
 
+        if constraint_name.startswith("EQ84_"):
+            i = constraint_name.split("_", 1)[1]
+            add(f"Q[{i}]", 1.0)
+            for h in self.sets.get("H", []):
+                add(f"C[{i},{h}]", -1.0)
+            add(f"CG[{i}]", -1.0)
+            add(f"INV[{i}]", -1.0)
+            add(f"VSTK[{i}]", -1.0)
+            add(f"DIT[{i}]", -1.0)
+            add(f"MRGN[{i}]", -1.0)
+            return result
+
+        if constraint_name.startswith("EQ85_"):
+            l = constraint_name.split("_", 1)[1]
+            add(f"LS[{l}]", 1.0)
+            for j in self.sets.get("J", []):
+                add(f"LD[{l},{j}]", -1.0)
+            return result
+
+        if constraint_name.startswith("EQ86_"):
+            k = constraint_name.split("_", 1)[1]
+            add(f"KS[{k}]", 1.0)
+            for j in self.sets.get("J", []):
+                add(f"KD[{k},{j}]", -1.0)
+            return result
+
+        if constraint_name == "EQ87":
+            add("IT", 1.0)
+            for h in self.sets.get("H", []):
+                add(f"SH[{h}]", -1.0)
+            for f in self.sets.get("F", []):
+                add(f"SF[{f}]", -1.0)
+            add("SG", -1.0)
+            add("SROW", -1.0)
+            return result
+
+        if constraint_name.startswith("EQ88_"):
+            i = constraint_name.split("_", 1)[1]
+            for j in self.sets.get("J", []):
+                if abs(self.equations.params.get("DSO0", {}).get((j, i), 0.0)) > 1e-12:
+                    add(f"DS[{j},{i}]", 1.0)
+            add(f"DD[{i}]", -1.0)
+            return result
+
+        if constraint_name.startswith("EQ89_"):
+            i = constraint_name.split("_", 1)[1]
+            for j in self.sets.get("J", []):
+                if abs(self.equations.params.get("EXO0", {}).get((j, i), 0.0)) > 1e-12:
+                    add(f"EX[{j},{i}]", 1.0)
+            add(f"EXD[{i}]", -1.0)
+            return result
+
         if constraint_name == "EQ90":
             add("GDP_BP", 1.0)
             add("TIPT", -1.0)
@@ -266,6 +405,48 @@ class PEPConstraintJacobianHarness:
             add("GDP_MP", 1.0)
             add("GDP_BP", -1.0)
             add("TPRCTS", -1.0)
+            return result
+
+        if constraint_name == "EQ92":
+            add("GDP_IB", 1.0)
+            add("TPRODN", -1.0)
+            add("TPRCTS", -1.0)
+            for l in self.sets.get("L", []):
+                for j in self.sets.get("J", []):
+                    if abs(self.equations.params.get("LDO0", {}).get((l, j), 0.0)) <= 1e-12:
+                        continue
+                    add(f"W[{l}]", -vars.LD.get((l, j), 0.0))
+                    add(f"LD[{l},{j}]", -vars.W.get(l, 0.0))
+            for k in self.sets.get("K", []):
+                for j in self.sets.get("J", []):
+                    if abs(self.equations.params.get("KDO0", {}).get((k, j), 0.0)) <= 1e-12:
+                        continue
+                    add(f"R[{k},{j}]", -vars.KD.get((k, j), 0.0))
+                    add(f"KD[{k},{j}]", -vars.R.get((k, j), 0.0))
+            return result
+
+        if constraint_name == "EQ93":
+            add("GDP_FD", 1.0)
+            for i in self.sets.get("I", []):
+                total_absorption = (
+                    sum(vars.C.get((i, h), 0.0) for h in self.sets.get("H", []))
+                    + vars.CG.get(i, 0.0)
+                    + vars.INV.get(i, 0.0)
+                    + vars.VSTK.get(i, 0.0)
+                )
+                add(f"PC[{i}]", -total_absorption)
+                for h in self.sets.get("H", []):
+                    add(f"C[{i},{h}]", -vars.PC.get(i, 0.0))
+                add(f"CG[{i}]", -vars.PC.get(i, 0.0))
+                add(f"INV[{i}]", -vars.PC.get(i, 0.0))
+                add(f"VSTK[{i}]", -vars.PC.get(i, 0.0))
+                if abs(self.equations.params.get("EXDO0", {}).get(i, 0.0)) > 1e-12:
+                    add(f"PE_FOB[{i}]", -vars.EXD.get(i, 0.0))
+                    add(f"EXD[{i}]", -vars.PE_FOB.get(i, 0.0))
+                if abs(self.equations.params.get("IMO0", {}).get(i, 0.0)) > 1e-12:
+                    add(f"PWM[{i}]", vars.e * vars.IM.get(i, 0.0))
+                    add("e", vars.PWM.get(i, 0.0) * vars.IM.get(i, 0.0))
+                    add(f"IM[{i}]", vars.PWM.get(i, 0.0) * vars.e)
             return result
 
         if constraint_name.startswith("EQ94_"):
@@ -308,6 +489,21 @@ class PEPConstraintJacobianHarness:
             if abs(vars.PIXINV) > 1e-12:
                 add("GFCF", -(1.0 / vars.PIXINV))
                 add("PIXINV", vars.GFCF / (vars.PIXINV ** 2))
+            return result
+
+        if constraint_name == "WALRAS":
+            walras_i = "agr" if "agr" in self.sets.get("I", []) else (self.sets.get("I", [None])[0])
+            if walras_i is None:
+                return result
+            add("LEON", 1.0)
+            add(f"Q[{walras_i}]", -1.0)
+            for h in self.sets.get("H", []):
+                add(f"C[{walras_i},{h}]", 1.0)
+            add(f"CG[{walras_i}]", 1.0)
+            add(f"INV[{walras_i}]", 1.0)
+            add(f"VSTK[{walras_i}]", 1.0)
+            add(f"DIT[{walras_i}]", 1.0)
+            add(f"MRGN[{walras_i}]", 1.0)
             return result
 
         return None
